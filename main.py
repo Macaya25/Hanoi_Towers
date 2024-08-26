35c168126724344c7de91c4a016c418675be6f69
Macaya25 <afmacaya@miuandes.cl>
2024-08-26 17:07:31 -0400

    Merged UI correctly.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import time
 import copy
+from UI import Palette, blit_text, draw_menu, draw_towers, draw_disks, draw_ptr, draw_game_over
 from solver import *
 
 pygame.init()
@@ -42,14 +43,9 @@ blue = (78, 162, 196)
 grey = (170, 170, 170)
 green = (77, 206, 145)
 
-
-def blit_text(screen, text, midtop, aa=True, font=None, font_name=None, size=None, color=(255, 0, 0)):
-    if font is None:                                    # font option is provided to save memory if font is
-        font = pygame.font.SysFont(font_name, size)     # already loaded and needs to be reused many times
-    font_surface = font.render(text, aa, color)
-    font_rect = font_surface.get_rect()
-    font_rect.midtop = midtop
-    screen.blit(font_surface, font_rect)
+current_theme = 1
+max_themes = 2
+colors = Palette(current_theme)
 
 
 def draw_button():
@@ -58,31 +54,40 @@ def draw_button():
 
 
 def menu_screen():  # to be called before starting actual game loop
-    global screen, n_disks, game_done
+    global screen, n_disks, game_done, colors, current_theme
     menu_done = False
     while not menu_done:  # every screen/scene/level has its own loop
-        screen.fill(white)
-        blit_text(screen, 'Towers of Hanoi', (323, 122), font_name='sans serif', size=90, color=grey)
-        blit_text(screen, 'Towers of Hanoi', (320, 120), font_name='sans serif', size=90, color=gold)
-        blit_text(screen, 'Use arrow keys to select difficulty:', (320, 220), font_name='sans serif', size=30, color=black)
-        blit_text(screen, str(n_disks), (320, 260), font_name='sans serif', size=40, color=blue)
-        blit_text(screen, 'Press ENTER to continue', (320, 320), font_name='sans_serif', size=30, color=black)
+        draw_menu(screen, n_disks, colors)
+
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
                     menu_done = True
                     game_done = True
-                if event.key == pygame.K_RETURN:
+                elif event.key == pygame.K_RETURN:
                     menu_done = True
-                if event.key in [pygame.K_RIGHT, pygame.K_UP]:
+                elif event.key == pygame.K_UP:
                     n_disks += 1
                     if n_disks > 6:
                         n_disks = 6
-                if event.key in [pygame.K_LEFT, pygame.K_DOWN]:
+                elif event.key == pygame.K_DOWN:
                     n_disks -= 1
                     if n_disks < 1:
                         n_disks = 1
-            if event.type == pygame.QUIT:
+                elif event.key == pygame.K_LEFT:
+                    current_theme -= 1
+                    if current_theme < 1:
+                        current_theme = max_themes
+                    colors = Palette(current_theme)
+                    draw_menu(screen, n_disks, colors)
+                elif event.key == pygame.K_RIGHT:
+                    current_theme += 1
+                    if current_theme > max_themes:
+                        current_theme = 1
+                    colors = Palette(current_theme)
+                    draw_menu(screen, n_disks, colors)
+
+            elif event.type == pygame.QUIT:
                 menu_done = True
                 game_done = True
         pygame.display.flip()
@@ -105,15 +110,6 @@ def game_over():  # game over screen
     sys.exit()  # console exit
 
 
-def draw_towers():
-    global screen
-    for xpos in range(40, 460+1, 200):
-        pygame.draw.rect(screen, green, pygame.Rect(xpos, 400, 160, 20))
-        pygame.draw.rect(screen, grey, pygame.Rect(xpos+75, 200, 10, 200))
-    blit_text(screen, 'Start', (towers_midx[0], 403), font_name='mono', size=14, color=black)
-    blit_text(screen, 'Finish', (towers_midx[2], 403), font_name='mono', size=14, color=black)
-
-
 def make_disks():
     global n_disks, disks
     disks = []
@@ -129,19 +125,6 @@ def make_disks():
         disks.append(disk)
         ypos -= height+3
         width -= 23
-
-
-def draw_disks():
-    global screen, disks
-    for disk in disks:
-        pygame.draw.rect(screen, blue, disk['rect'])
-    return
-
-
-def draw_ptr():
-    ptr_points = [(towers_midx[pointing_at]-7, 440), (towers_midx[pointing_at]+7, 440), (towers_midx[pointing_at], 433)]
-    pygame.draw.polygon(screen, red, ptr_points)
-    return
 
 
 def check_won():
@@ -312,9 +295,9 @@ while not game_done:
                 first_move = True  # Button disappears after the first click
 
     screen.fill(white)
-    draw_towers()
-    draw_disks()
-    draw_ptr()
+    draw_towers(screen, towers_midx, colors)
+    draw_disks(screen, disks, colors)
+    draw_ptr(screen, towers_midx, pointing_at, colors)
     if not first_move:
         draw_button()
     blit_text(screen, 'Steps: '+str(steps), (320, 20), font_name='mono', size=30, color=black)
